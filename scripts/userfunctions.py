4fafb6cb933d0204c498e59542c38b346c1d8c1f
SimonBiggs <mail@simonbiggs.net>
2014-08-05 17:21:01 +1000

    end of wednesday

--- scripts/userfunctions.py
+++ scripts/userfunctions.py
@@ -1,12 +1,13 @@
 import numpy as np
+from scipy.interpolate import SmoothBivariateSpline
 
 
-def single_angle_gap(xTest,yTest,xData,yData):
-    xi = (xData - xData.min()) / xData.ptp()
-    yi = (yData - yData.min()) / yData.ptp()
+def single_angle_gap(xTest,yTest,xData,yData,xScale,yScale):
+    xi = xScale * (xData - xData.min()) / xData.ptp()
+    yi = yScale * (yData - yData.min()) / yData.ptp()
     
-    xVal = (xTest - xData.min()) / xData.ptp()
-    yVal = (yTest - yData.min()) / yData.ptp()
+    xVal = xScale * (xTest - xData.min()) / xData.ptp()
+    yVal = yScale * (yTest - yData.min()) / yData.ptp()
     
     dx = xi - xVal
     dy = yi - yVal
@@ -29,12 +30,12 @@ def single_angle_gap(xTest,yTest,xData,yData):
 
 
 		
-def angle_gap(xTest,yTest,xData,yData):
+def angle_gap(xTest,yTest,xData,yData,xScale,yScale):
     
     dim = np.core.fromnumeric.shape(xTest)  
     
     if np.size(dim) == 0:
-        gap = single_angle_gap(xTest,yTest,xData,yData)
+        gap = single_angle_gap(xTest,yTest,xData,yData,xScale,yScale)
         
         return gap
     
@@ -44,13 +45,63 @@ def angle_gap(xTest,yTest,xData,yData):
     
     if np.size(dim) == 1:
         for i in range(dim[0]):
-            gap[i] = single_angle_gap(xTest[i],yTest[i],xData,yData)
+            gap[i] = single_angle_gap(xTest[i],yTest[i],xData,yData,xScale,yScale)
             
         return gap
     
     
     for i in range(dim[0]):
         for j in range (dim[1]):
-            gap[i,j] = single_angle_gap(xTest[i,j],yTest[i,j],xData,yData)
+            gap[i,j] = single_angle_gap(xTest[i,j],yTest[i,j],xData,yData,xScale,yScale)
 
     return gap
+
+		
+		
+def single_fit_give(xTest,yTest,xData,yData,zData):
+    
+    initialFitReturn = SmoothBivariateSpline(xData,yData,zData).ev(xTest,yTest)
+    
+    adjXData = np.append(xData,xTest)
+    adjYData = np.append(yData,yTest)
+    
+    posAdjZData = np.append(zData,initialFitReturn + 1)
+    negAdjZData = np.append(zData,initialFitReturn - 1)
+    
+    posFitReturn = SmoothBivariateSpline(adjXData,adjYData,posAdjZData).ev(xTest,yTest)
+    negFitReturn = SmoothBivariateSpline(adjXData,adjYData,negAdjZData).ev(xTest,yTest)
+    
+    posGive = posFitReturn - initialFitReturn
+    negGive = initialFitReturn - negFitReturn
+    
+    give = np.mean([posGive, negGive])
+    
+    return give
+	
+	
+
+def fit_give(xTest,yTest,xData,yData,zData):
+    
+    dim = np.core.fromnumeric.shape(xTest)
+    
+    if np.size(dim) == 0:
+        give = single_fit_give(xTest,yTest,xData,yData,zData)
+        
+        return give
+    
+    
+    give = np.zeros(dim)
+    
+    
+    if np.size(dim) == 1:
+        for i in range(dim[0]):
+            give[i] = single_fit_give(xTest[i],yTest[i],xData,yData,zData)
+            
+        return give
+    
+    
+    for i in range(dim[0]):
+        for j in range (dim[1]):
+            give[i,j] = single_fit_give(xTest[i,j],yTest[i,j],xData,yData,zData)
+
+    return give
